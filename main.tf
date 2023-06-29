b83a218b373f86b2010e1be2f0586c5ff1df3dcb
Ganapath1 <99318790+Ganapath1@users.noreply.github.com>
2023-06-29 11:11:50 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "myawsbucket12345554555"
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
